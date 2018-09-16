c2c05afd9f9fcbd4a319206df9348b6e7c4d103d
jlander <lander.jon9@gmail.com>
2018-09-16 12:10:04 -0400

    different approach

--- pdfparser.py
+++ pdfparser.py
@@ -7,10 +7,47 @@ import PyPDF2
 def main():
     words = []
     time = []
-    classType = []
+    instructor = []
+    classType = [()]
     words = getWords()
-    seperateTimeWords(words,time,classType)
-    print(sorted(classType))
+    sections = getSections(words)
+    cleanSections(sections)
+    for i in sections:
+        print(i)
+    """
+    seperateTimeWords(words,time,classType,instructor)
+    print(len(classType))
+    classType = getClassNumber(classType,words, time, instructor)
+    for c in classType:
+        print(c)
+    print(len(words))
+    print(len(time))
+    print(len(instructor))
+    print(len(classType))
+    """
+
+
+def cleanSections(sections):
+    i = 0
+    while i < len(sections)-1:
+        if sections[i][0] == 'Report':
+            del sections[i]
+        i = i + 1
+
+def getSections(words):
+    add = False
+    section = []
+    sections = []
+    for w in words:
+        section.append(w)
+        if '__________________' in w:
+            sections.append(section)
+            section = []
+    return sections
+
+
+
+
 
 def createFile():
     newfile = open('test.txt', 'w')
@@ -41,36 +78,50 @@ def getWords():
     print(words)
     return words
 
-def seperateTimeWords(words,time, classType):
+def seperateTimeWords(words,time, classType,instructor):
+    del classType[0]
     keywords = ['C01)', 'C01', 'ITV', 'TBA', 'C01/', 'C02', 'C02/', 'C02)', 'V01', 'V01/', 'V01)', 'UNDG', '(E', '(E.',
                 'X01.', 'X01', 'NOT', 'THE', 'AH,']
     i = 0
     while i < len(words):
         try:
-            if '0am' in words[i] or '5am' in words[i] or '0pm' in words[i] or '5pm' in words[i]:
+            if 'Time' in words[i-2] and '0am' in words[i] or '5am' in words[i] or '0pm' in words[i] or '5pm' in words[i]:
                 time.append(words[i - 1] + ':' + words[i] + '-' + words[i + 2] + ":" + words[i + 3])
                 i = i + 4
+            if 'Time' in words[i] and 'TBA' in words[i+1]:
+                time.append('TBA')
+            if 'Instructor' in words[i]:
+                instructor.append(words[i+1])
+
         except IndexError:
             print('Could not finish. Did not read enough characters')
-
-        if words[i].isupper() and 5 > len(words[i]) > 2:
-            classType.append(words[i])
+        add = True
+        for x in keywords:
+            if words[i] == x:
+                add = False
+        if words[i].isupper() and 5 > len(words[i]) > 2 and add:
+            classType.append((words[i], i+1))
         i = i + 1
-    print(len(classType))
     i = 0
-    while i < len(classType) - 1:
-        for blacklist in keywords:
-            if classType[i] == blacklist:
-                del classType[i]
-            if classType[i-1] == blacklist:
-                del classType[i-1]
-            if classType[i+1] == blacklist:
-                del classType[i+1]
+
+def getClassNumber(classType, words,time, instructor):
+    holder = [x[1] for x in classType]
+    cn = [x[0]for x in classType]
+    i = 0
+    l = [()]
+    del l[0]
+    for h in holder:
+        future = words[h][:3]
+        if future == ' ' or future.isdigit() and time:
+            l.append((cn[i], future, words[h][3:6], holder[i]))
+            holder[i] = future
+        else:
+            del cn[i]
+            del holder[i]
         i = i + 1
 
-
-    for classT in classType:
-        print(classT)
+    return l
 
 #Calling the functions.
 main()
+
